735f0844da1afd5bf6cd40e3c8f53553cf50ac3e
HiteshJoshi2162 <raajjoshi3252@gmail.com>
2026-04-27 22:52:04 +0530

    Update extract.routes.js

--- extract.routes.js
+++ extract.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const extractController = require('./extract.controller');
-const { extractLimiter } = require('./rateLimiter');
+const extractController = require('../controllers/extract.controller');
+const { extractLimiter } = require('../middleware/rateLimiter');
 
 router.post('/extract', extractLimiter, extractController.extractMedia);
+router.get('/download', extractLimiter, extractController.downloadMedia);
 
 module.exports = router;
