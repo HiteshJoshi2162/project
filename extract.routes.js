16ddeb30847dc0390af10ba72e502a41aa2518a9
HiteshJoshi2162 <raajjoshi3252@gmail.com>
2026-04-27 14:11:19 +0530

    Add files via upload

---
+++ extract.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const extractController = require('../controllers/extract.controller');
+const { extractLimiter } = require('../middleware/rateLimiter');
+
+router.post('/extract', extractLimiter, extractController.extractMedia);
+
+module.exports = router;
