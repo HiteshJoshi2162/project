b42b462f30505aa7a9e1f9b05234429013f5be2e
HiteshJoshi2162 <raajjoshi3252@gmail.com>
2026-04-27 22:54:51 +0530

    Update extract.controller.js

--- extract.controller.js
+++ extract.controller.js
@@ -1,5 +1,5 @@
-const extractService = require('../services/extract.service');
-const { validateUrl } = require('../utils/validator');
+const extractService = require('./extract.service');
+const { validateUrl } = require('./validator');
 
 /**
  * Common logic for processing extraction requests
