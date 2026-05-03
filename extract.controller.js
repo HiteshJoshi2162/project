d63bfad85bf3239137270a6f8141b5344a06ea23
HiteshJoshi2162 <raajjoshi3252@gmail.com>
2026-05-03 21:22:23 +0530

    Update extract.controller.js

--- extract.controller.js
+++ extract.controller.js
@@ -1,5 +1,5 @@
-const extractService = require('./extract.service');
-const { validateUrl } = require('./validator');
+const extractService = require('../services/extract.service');
+const { validateUrl } = require('../utils/validator');
 
 const processExtraction = async (url, res) => {
     try {
